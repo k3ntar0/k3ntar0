8c898685de44bb84668d237762d4f754cdbc5353
Kentaro Iwata <k3ntar01wata@gmail.com>
2021-05-08 17:17:15 +0900

    update profile

--- pages/index.js
+++ pages/index.js
@@ -10,53 +10,26 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hi there👋
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          I'm Kentaro Iwata.
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://twitter.com/k3ntar0" className="card">
+            <h3>Twitter 🐦</h3>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="https://github.com/k3ntar0" className="card">
+            <h3>GitHub 🐙</h3>
           </a>
         </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+        <p>COPYRIGHT © 2021 Kentaro Iwata</p>
       </footer>
 
       <style jsx>{`
@@ -168,7 +141,6 @@ export default function Home() {
         }
 
         .card h3 {
-          margin: 0 0 1rem 0;
           font-size: 1.5rem;
         }
 
